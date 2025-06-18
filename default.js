b1c94544650a86c30c6b16fcefc7b5c0e1d52af5
Karel Hacha-Pical <hacha.pical@gmail.com>
2025-06-18 08:12:24 +0200

    improve page navigation

--- default.js
+++ default.js
@@ -1,58 +1,23 @@
-let index = 0;
-let articles = null;
-let next_btn = null;
-let prev_btn = null;
-
-function enable(element){
-    element.classList.remove("disable");    
-}
-
-function disable(element){
-    element.classList.add("disable");    
-}
-
-function hide(index){
-    let element = articles[index];
-    element.classList.remove("visible")   
-}
-
-function show(index){
-    let element = articles[index];
-    element.classList.add("visible");
-}
-
-function check_nav(){
-    enable(prev_btn);
-    enable(next_btn);
-    if (index == 0) disable(prev_btn);
-    if (index + 2 == articles.length) disable(next_btn);
-}
+let swiper = null;
 
 function prev(){
-    if (index - 1 < 0){        
-        return;
-    }
-    enable(next_btn);
-    hide(index--);
-    show(index);
-    check_nav()
+    swiper.slidePrev();
 }
 
 function next(){
-    if (index + 2 >= articles.length){        
-        return;
-    }
-    enable(prev_btn);
-    hide(index++);
-    show(index);
-    check_nav()
+    swiper.slideNext();
 }
+
 window.addEventListener('load', function () {
-   const main = document.getElementById("main");
-   articles = main.children;
-   next_btn = document.getElementById("next");
-   prev_btn = document.getElementById("prev");
-   
-   show(index);
-   check_nav();
+    
+    swiper = new Swiper('.swiper', {
+        
+         keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+          },
+          
+    });
+
 });    
+
