67f4e23a9ccb1d51b2263ca57575566f1c02a151
Karel Hacha-Pical <hacha.pical@gmail.com>
2025-06-18 14:40:43 +0200

    add dark theme

--- default.js
+++ default.js
@@ -21,3 +21,37 @@ window.addEventListener('load', function () {
 
 });    
 
+/* ------------------------------------------------------------------ */
+   const THEME = true;
+/* ------------------------------------------------------------------ */
+
+function applyTheme(dark) {
+   let body = document.body, html = document.getElementsByTagName('html')[0];
+   if (dark) {
+      body.classList.add('dark-mode');
+      html.classList.add('dark-mode');
+   } else {
+      body.classList.remove('dark-mode');
+      html.classList.remove('dark-mode');
+   }
+   return dark;
+}
+
+function saveTheme(dark) {
+   localStorage.setItem("dark-mode", dark ? "enabled" : null);
+}
+
+function darkMode(){
+   return (localStorage.getItem("dark-mode") == "enabled");
+}
+
+function setTheme() {
+   if (!THEME) return;
+   applyTheme(darkMode());
+}
+
+function switchTheme() {
+   saveTheme(applyTheme(!darkMode()));
+}
+
+/* ------------------------------------------------------------------ */
